4e7d5288dd07026121269a4a432fb72eb2749e56
刘东 <liudong@liudongdeMacBook-Pro.local>
2017-12-09 16:14:50 +0800

    Initial Commit

---
+++ GCDTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDTimer
+//
+//  Created by 刘东 on 2017/12/9.
+//  Copyright © 2017年 刘东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

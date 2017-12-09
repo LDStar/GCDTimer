61cf5cb7325d1cc05ccc2fa214c4badcd9006534
刘东 <liudong@liudongdeMacBook-Pro.local>
2017-12-09 16:19:32 +0800

    first commit

---
+++ GCDTimer/LDTimer.h
@@ -0,0 +1,13 @@
+//
+//  LDTimer.h
+//  GCDTimer
+//
+//  Created by 刘东 on 2017/12/9.
+//  Copyright © 2017年 刘东. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LDTimer : NSObject
+
+@end
---
+++ GCDTimer/LDTimer.m
@@ -0,0 +1,13 @@
+//
+//  LDTimer.m
+//  GCDTimer
+//
+//  Created by 刘东 on 2017/12/9.
+//  Copyright © 2017年 刘东. All rights reserved.
+//
+
+#import "LDTimer.h"
+
+@implementation LDTimer
+
+@end
